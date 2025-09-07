a6372f4bb0cafe156de27f28d0e8f70abe5a3a34
ekc0106 <ekc0106@naver.com>
2025-09-08 01:46:49 +0900

    mod: 구글드라이브 연동5

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -42,11 +42,7 @@ export async function POST(request: NextRequest) {
         private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
         private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        client_id: process.env.GOOGLE_CLIENT_ID,
-        auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-        token_uri: 'https://oauth2.googleapis.com/token',
-        auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-        client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${process.env.GOOGLE_CLIENT_EMAIL}`
+        client_id: process.env.GOOGLE_CLIENT_ID
       },
       scopes: ['https://www.googleapis.com/auth/drive.file']
     });
