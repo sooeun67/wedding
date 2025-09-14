e86ec5ddfd0ec55738b845cacb3fd85baf205356
ekc0106 <ekc0106@naver.com>
2025-09-14 16:23:21 +0900

    사진추가

--- src/config/wedding-config.ts
+++ src/config/wedding-config.ts
@@ -88,10 +88,9 @@ export const weddingConfig = {
     position: "middle" as GalleryPosition, // "middle" (현재 위치) 또는 "bottom" (맨 하단) 선택
     images: [
       "/images/gallery/01.jpg",
-      "/images/gallery/02.jpg",
+      "/images/gallery/02_1.jpg",
       "/images/gallery/03.jpg",
       "/images/gallery/04.jpg",
-      "/images/gallery/05.jpg",
       "/images/gallery/06.jpg",
       "/images/gallery/07.jpg",
       "/images/gallery/08.jpg",
